56d8abc0d285d6d844060d24fcf79f355676f0a9
morrss <grishkovea@gmail.com>
2024-08-16 01:50:35 +0300

    сделал отправку данных из форм на сервер, обработал их, и сделал пока что не очень - в конце запроса перенаправляю юзера на странице входа, иначе, будет пустая страница из ответа сервлета, как это грамотно решить - хз

---
+++ src/main/java/com/example/labee/servlets/AuthServlet.java
@@ -0,0 +1,21 @@
+package com.example.labee.servlets;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/auth")
+public class AuthServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        System.out.println(request.getParameter("username"));
+        System.out.println(request.getParameter("firstname"));
+        System.out.println(request.getParameter("lastname"));
+        System.out.println(request.getParameter("password"));
+        response.sendRedirect("/auth-page");
+    }
+
+}
