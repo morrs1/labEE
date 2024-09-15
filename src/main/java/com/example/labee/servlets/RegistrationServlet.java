7407a71a0eb85594c1aba98d81cebbe35c9405e2
morrs <grishkovea@gmail.com>
2024-09-15 15:48:11 +0300

    сделал первый запрос для регистрации, все работает

--- src/main/java/com/example/labee/servlets/RegistrationServlet.java
+++ src/main/java/com/example/labee/servlets/RegistrationServlet.java
@@ -1,6 +1,9 @@
 package com.example.labee.servlets;
 
+import com.example.labee.model.core.helpers.HashPassword;
+import com.example.labee.model.core.helpers.JSONParser;
 import com.example.labee.model.core.helpers.XMLParser;
+import com.example.labee.model.schemas.User;
 import com.example.labee.model.schemas.XMLUsers;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -8,27 +11,40 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 
 @WebServlet("/registration")
 public class RegistrationServlet extends HttpServlet {
-    XMLUsers users;
+    private XMLUsers users;
+    private File xmlFile;
+
     @Override
-    public void init(){
-        users = (XMLUsers) XMLParser.deserialize(new File(
+    public void init() {
+        xmlFile = new File(
                 new File(
                         getServletContext().getRealPath("/")
                 ).getParentFile().getParentFile().getAbsolutePath()
                         + "/src/main/java/com/example/labee/resources/xml/accounting-information.xml"
-        ), new XMLUsers());
+        );
+        users = (XMLUsers) XMLParser.deserialize(xmlFile, new XMLUsers());
     }
 
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
-       if (users.getByLogin(req.getParameter("login")) == null){
-           System.out.println(req.getParameter("login"));
-       }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Map<String, String> requestParameters = JSONParser.parse(request.getReader());
+        if (users.getByLogin(requestParameters.get("login")) == null) {
+            users.getUsersList().add(new User(
+                            requestParameters.get("login"),
+                            HashPassword.hashPassword(requestParameters.get("password"))
+                    )
+            );
+            XMLParser.serialize(xmlFile, users);
+        }
+        System.out.println(users.getUsersList());
     }
 
 }
