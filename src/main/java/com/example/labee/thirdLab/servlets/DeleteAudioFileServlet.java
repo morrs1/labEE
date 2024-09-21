4ebac4398555a7088be6f0d7902bc8aaeae71a65
morrs <grishkovea@gmail.com>
2024-09-21 21:57:25 +0300

    починил работу с файлом

--- src/main/java/com/example/labee/thirdLab/servlets/DeleteAudioFileServlet.java
+++ src/main/java/com/example/labee/thirdLab/servlets/DeleteAudioFileServlet.java
@@ -16,13 +16,7 @@ public class DeleteAudioFileServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         AudioFileDAO audioFileDAO = new DAOCreator().createAudiFileDAO();
-        System.out.println(audioFileDAO.getAudioFileList());
         audioFileDAO.deleteAudioFile(request.getParameter("id"));
-        System.out.println(audioFileDAO.getAudioFileList());
-        System.out.println(new File(
-                getServletContext().getRealPath("/")
-        ).getParentFile().getParentFile().getAbsolutePath()
-                + "/src/main/resources/XML/audioFiles.xml");
         new DAOCreator().serialize(audioFileDAO);
         System.out.println("Удаление прошло успешно");
         request.getRequestDispatcher("thirdLab/main-page-third-lab.jsp").forward(request, response);
