b00ef4984ca476a7438a0d6ed75fbadc283d3e0c
morrs <grishkovea@gmail.com>
2024-09-21 20:09:15 +0300

    сделал удаление данных с помощью веба, но удаление на вебе не отображается

--- src/main/java/com/example/labee/thirdLab/model/helpers/DAOCreator.java
+++ src/main/java/com/example/labee/thirdLab/model/helpers/DAOCreator.java
@@ -2,16 +2,25 @@ package com.example.labee.thirdLab.model.helpers;
 
 import com.example.labee.model.core.helpers.XMLParser;
 import com.example.labee.thirdLab.schemas.AudioFileDAO;
+import lombok.Getter;
 
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 public class DAOCreator {
-    public static AudioFileDAO createAudiFileDAO(){
+
+
+    public static AudioFileDAO createAudiFileDAO() {
         InputStream streamOfAudioFile = DAOCreator.class.getClassLoader().getResourceAsStream("XML/audioFiles.xml");
         if (streamOfAudioFile == null) {
             throw new RuntimeException("Файл не найден: XML/audioFiles.xml");
         }
         return (AudioFileDAO) XMLParser.deserialize(streamOfAudioFile, new AudioFileDAO());
+    }
+
+    public static FileOutputStream getOutputStream() throws IOException {
+        return new FileOutputStream("src/main/resources/XML/audioFiles.xml");
     }
 }
---
+++ src/main/java/com/example/labee/thirdLab/servlets/DeleteAudioFileServlet.java
@@ -0,0 +1,32 @@
+package com.example.labee.thirdLab.servlets;
+
+import com.example.labee.model.core.helpers.XMLParser;
+import com.example.labee.thirdLab.model.helpers.DAOCreator;
+import com.example.labee.thirdLab.schemas.AudioFileDAO;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.File;
+import java.io.IOException;
+
+@WebServlet("/delete")
+public class DeleteAudioFileServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        AudioFileDAO audioFileDAO = DAOCreator.createAudiFileDAO();
+        audioFileDAO.deleteAudioFile("2");
+        XMLParser.serialize(new File(
+                new File(
+                        getServletContext().getRealPath("/")
+                ).getParentFile().getParentFile().getAbsolutePath()
+                        + "/src/main/resources/XML/audioFiles.xml"
+        ), audioFileDAO);
+        System.out.println("Удаление прошло успешно");
+        request.getRequestDispatcher("thirdLab/main-page-third-lab.jsp").forward(request, response);
+
+    }
+}
+
