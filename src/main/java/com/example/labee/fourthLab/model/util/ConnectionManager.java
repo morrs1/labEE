b2238a65bafa690b07a819b6b01facba4c5435a1
morrs <grishkovea@gmail.com>
2024-09-23 21:09:44 +0300

    добавил класс для подключения и загрузил драйвер в Maven

---
+++ src/main/java/com/example/labee/fourthLab/model/util/ConnectionManager.java
@@ -0,0 +1,59 @@
+package com.example.labee.fourthLab.model.util;
+
+import lombok.NoArgsConstructor;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+@NoArgsConstructor
+public class ConnectionManager {
+    private static final String URL_KEY = "db.url";
+    private static final String USER_KEY = "db.name";
+    private static final String PASSWORD_KEY = "db.password";
+    private static final String POOL_SIZE_KEY = "db.pool.size";
+    private static final Integer DEFAULT_POOL_SIZE = 10;
+    private static BlockingQueue<Connection> pool;
+
+    static {
+        loadDriver();
+        initConnectionPool();
+    }
+
+    private static void initConnectionPool() {
+        String poolSize = PropertiesUtil.getProperty(POOL_SIZE_KEY);
+        int size = poolSize == null ? DEFAULT_POOL_SIZE : Integer.parseInt(poolSize);
+        pool = new ArrayBlockingQueue<>(size);
+        for (int i = 0; i < size; i++) {
+            pool.add(open());
+        }
+    }
+
+    public static Connection get() {
+        try {
+            return pool.take();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Connection open() {
+        try {
+            return DriverManager.getConnection(PropertiesUtil.getProperty(URL_KEY),
+                    PropertiesUtil.getProperty(USER_KEY),
+                    PropertiesUtil.getProperty(PASSWORD_KEY));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void loadDriver() {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
