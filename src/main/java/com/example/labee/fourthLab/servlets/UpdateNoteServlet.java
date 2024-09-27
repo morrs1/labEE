9f4851b7a42dcbf06223c56955d5057a3e6acf68
morrs <grishkovea@gmail.com>
2024-09-27 19:56:29 +0300

    добавил сервлет для обновления записей

---
+++ src/main/java/com/example/labee/fourthLab/servlets/UpdateNoteServlet.java
@@ -0,0 +1,39 @@
+package com.example.labee.fourthLab.servlets;
+
+import com.example.labee.fourthLab.database.entity.Displayable;
+import com.example.labee.fourthLab.model.core.DAOFactory;
+import com.example.labee.fourthLab.model.core.EntityFactory;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.sql.SQLException;
+import java.util.Arrays;
+
+@WebServlet("/update-note")
+public class UpdateNoteServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getParameterMap().forEach((x,y)->System.out.println(x+" "+ Arrays.toString(y)));
+        Displayable newEntity = EntityFactory.create(request.getParameter("typeOfTable"), request);
+
+        try {
+            DAOFactory.getDAO(request.getParameter("typeOfTable")).update(newEntity);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            String typeOfTable = URLEncoder.encode(request.getParameter("typeOfTable"), StandardCharsets.UTF_8);
+            response.sendRedirect("main-page-fourth-lab?typeOfTable=" + typeOfTable);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace(); // Обработка исключения
+        }
+    }
+}
